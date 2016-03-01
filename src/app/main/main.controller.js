a6227f0b6619c10098d0668e16763845986f7950
Ali Barın <ali.barin53@gmail.com>
2016-03-01 02:37:22 +0200

    Remove mock from textarea

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -12,10 +12,6 @@ export class MainController {
   }
 
   submitCsv() {
-    this.csvData = `Id;Company Name;Founder;City;Country;Postal Code; Street;Photo;Home Page;Garage Latitude;Garage Longitude
-    1;Google;Larry Page & Sergey Brin;Mountain View;USA;CA 94043;1600 Amphitheatre Pkwy;https://s.yimg.com/uy/build/images/sohp/backgrounds/bottom5.jpg;http://google.com;37.457674;-122.163452
-    2;Apple;Steve Jobs & Steve Wozniak;Cupertino;USA;CA 95014;1 Infinite Loop;https://s.yimg.com/uy/build/images/sohp/inspiration/bliss3.jpg;http://apple.com;37.3403188;-122.0581469
-    3;Microsoft;Bill Gates;Redmond;USA;WA 98052-7329;One Microsoft Way;https://s.yimg.com/uy/build/images/sohp/hero/carlin-gill-valley3.jpg;http://microsoft.com;37.472189;-122.190191`;
     let rows = this.csvData.split('\n');
 
     let csvObject = {
